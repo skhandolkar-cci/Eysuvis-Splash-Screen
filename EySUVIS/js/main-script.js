3c5f30f95aea115d4f932ab2c1f8629c5e014e74
sphatak-cci <shreyas.phatak@creativecapsule.com>
2021-01-07 19:35:24 +0530

    added onclick function for up and down arrow icon for info section

--- EySUVIS/js/main-script.js
+++ EySUVIS/js/main-script.js
@@ -1,19 +1,36 @@
-window.onscroll = function() {myFunction()};
- function myFunction() {
-    if (pageYOffset > 140) {
+window.onscroll = function() {scrollFunction()};
+ function scrollFunction() {
+    var yscroll = $(".safety").scrollTop();
+    if (pageYOffset >= yscroll) {
       $(".safety-info").addClass("d-none");
-      $(".safety").removeClass("d-none")
+      $(".safety").removeClass("d-none");
     }
-    else {
-      $(".safety-info").removeClass("d-none");
-      $(".safety").addClass("d-none");
+    if (pageYOffset <= yscroll) {
+        $(".safety-info").removeClass("d-none");
+        $(".safety").addClass("d-none");
     }
  }
- function onScroll() { 
-     var sectionTo = $(".safety-information");
+ function openSection() { 
+     var sectionTo = $(".safety");
      $(".safety-info").addClass("d-none");
      $(".safety").removeClass("d-none");
+     $(".safety").addClass("sticky-info");
+     $('h1').addClass("d-none");
+     $('header').addClass("d-none");
+     $('footer').addClass("d-none");
+     $(".down-arrow-icon").removeClass("d-none");
      $('html, body').animate({
      scrollTop: $(sectionTo).offset().top
-     }, 500);
+     }, 0);
  }
+ function closeSection() { 
+  $(".safety-info").removeClass("d-none");
+  $(".down-arrow-icon").addClass("d-none");
+  $('h1').removeClass("d-none");
+  $("header").removeClass("d-none")
+  $('footer').removeClass("d-none");
+  $(".safety").removeClass("sticky-info");
+  $('html, body').animate({
+  scrollTop: $('header').offset().top
+  },0);
+}
